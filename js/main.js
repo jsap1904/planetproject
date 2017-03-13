e060413eb14067022923c0b414f8c5a510a731c7
jsap1904 <sapinoso.j@gmail.com>
2017-03-12 23:19:27 -0700

    first

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var planets = [
+	{
+		planet: "earth",
+		info: "Earth, otherwise known as the world, is the third planet from the Sun and the only object in the Universe known to harbor life. It is the densest planet in the Solar System and the largest of the four terrestrial planets.",
+		image: "img/earth.jpg",
+	},
+	{
+		planet: "pluto",
+		info: "Emotion, Diplomatic, Intensity, Impulsive, Selective",
+		image: "img/pluto.jpg"
+	},
+	{
+		planet: "mars",
+		info: "Determination, Dominance, Perservering, Practical, Willful",
+		image: "img/mars.jpg"
+	},
+	{
+		planet: "jupiter",
+		info: "Communication, Indecision, Inquisitive, Intelligent, Changeable",
+		image: "img/jupiter.jpg"
+	},
+	{
+		planet: "neptune",
+		info: "Ruling, Warmth, Generosity, Faithful, Initiative",
+		image: "img/neptune.jpg"
+	},
+	{
+		planet: "uranus",
+		info: "Balance, Justice, Truth, Beauty, Perfection",
+		image: "img/uranus.jpg"
+	},
+	{
+		planet: "venus",
+		info: "Fluctuation, Depth, Imagination, Reactive, Indecisive",
+		image: "img/venus.jpg"
+	},
+	{
+		planet: "mercury",
+		info: "Philosophical, Motion, Experimentation, Optimism",
+		image: "img/mercury.jpg"
+	},
+	{
+		planet: "saturn",
+		info: "Transient, Self-Willed, Purposeful, Unyielding",
+		image: "img/saturn.jpg"
+	}
+]
